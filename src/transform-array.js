38d89804949f87c1ee6b4e20570d12f9763d21a2
Volha <steblikova@gmail.com>
2022-10-16 00:19:09 +0400

    solve what-season task

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,19 +3,32 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ * * `--discard-next` excludes the next element of the array from the transformed array.
+* `--discard-prev` excludes the previous element of the array from the transformed array.
+* `--double-next` duplicates the next element of the array in the transformed array.
+* `--double-prev` duplicates the previous element of the array in the transformed array.
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const DISCARD_NEXT = '--discard-next';
+const DISCARD_PREV = '--discard-prev';
+const DOUBLE_NEXT = '--double-next';
+const DOUBLE_PREV = '--double-prev';
+function transform(arr) {
+  const newArray = arr.slice(0);
+  newArray.forEach((element, index) => {
+ if (element === DOUBLE_NEXT) {
+
+ }
+
+  });
+
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -1,19 +1,58 @@
-const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const WINTER = 'winter';
+const SPRING = 'spring';
+const SUMMER = 'summer';
+const AUTUMN = 'autumn';
+const seasonMathes = {
+  0: WINTER,
+  1: WINTER,
+  2: SPRING,
+  3: SPRING,
+  4: SPRING,
+  5: SUMMER,
+  6: SUMMER,
+  7: SUMMER,
+  8: AUTUMN,
+  9: AUTUMN,
+  10: AUTUMN,
+  11: WINTER,
+}
+function getSeason(date) {
+  if ( date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+  try {
+    date.getTime();
+  } catch {
+    throw new Error('Invalid date!');
+  }
+  return seasonMathes[date.getMonth()]
+  // if ( date.getMonth() === 0 || date.getMonth() === 1 || date.getMonth() === 11 ) {
+  //   return 'winter';
+  // };
+  // if ( date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) {
+  //   return 'spring';
+  // };
+  // if ( date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7) {
+  //   return 'summer';
+  // }
+  //   if ( date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) {
+  //     return 'fall';
+  // }
+
+
+  return 'false';
 }
 
 module.exports = {
